684df10ddfa9c663c5989632d440c0acf108df81
sehara <shingo.b.ehara@gmail.com>
2026-01-05 09:31:39 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+const courseInfo = {
+    "PE VC Lab": { due: "Sunday, Jan 5 at 11:59 PM" },
+    "Managing in Organizations": { due: "Tuesday, Jan 6 at 8:30 AM" },
+    "Negotiation": { due: "Thursday, Jan 8 at 8:30 AM" },
+    "Corporate Governance": { due: "Thursday, Jan 9 at 12:01 AM" }
+};
+
+const courseData = {
+    "Sunday, January 5, 2026": {
+        "PE VC Lab|||Due: Sunday, Jan 5 at 11:59 PM": {
+            submit: [{ title: "Assignment 1: Submit Student Bio", time: 0.5, points: 0, link: "https://canvas.uchicago.edu" }],
+            reading: [], required: [], optional: []
+        }
+    }
+};
